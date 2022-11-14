ccf05c8e289a09cc252b3263f4e4442661bbd1b1
JoaquinMF <kino_1992@hotmail.com>
2022-11-14 14:11:30 +0100

    comit

--- ejercicio2/src/application/InicioSesionController.java
+++ ejercicio2/src/application/InicioSesionController.java
@@ -1,7 +1,11 @@
 package application;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 
 public class InicioSesionController {
@@ -21,7 +25,25 @@ public class InicioSesionController {
 	@FXML
 	private Label label4;
 	
+	@FXML
+	private TextField txt1;
 	
+	@FXML
+	private PasswordField pass1;
+	
+	@FXML
+	private Button button1;
+	
+	@FXML
+	public void loginUsuario(ActionEvent event) {
+		
+		
+		label4.setText("Bienvenido: "+txt1.getText());
+		txt1.setText(null);
+		pass1.setText(null);
+		
+		
+	}
 	
 	
 }
