1e3b82966ded379e9ac6b23936b29d48cc75c823
JoaquinMF <kino_1992@hotmail.com>
2022-11-14 14:02:42 +0100

    commit1

---
+++ ejercicio2/src/application/InicioSesionController.java
@@ -0,0 +1,27 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class InicioSesionController {
+	
+	@FXML
+	private ImageView img1;
+	
+	@FXML
+	private Label label1;
+	
+	@FXML
+	private Label label2;
+	
+	@FXML 
+	private Label label3;
+	
+	@FXML
+	private Label label4;
+	
+	
+	
+	
+}
