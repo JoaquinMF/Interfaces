cbbd6717852da6c6b80ee3c053de89d16075ba34
JoaquinMF <kino_1992@hotmail.com>
2022-11-14 14:24:52 +0100

    commit2

--- ejercicio2/src/application/Main.java
+++ ejercicio2/src/application/Main.java
@@ -3,6 +3,7 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
@@ -15,6 +16,7 @@ public class Main extends Application {
 			Scene scene = new Scene(root,700,500);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.getIcons().add(new Image("/application/icono.png"));
 			primaryStage.setTitle("Inicio de Sesion");
 			primaryStage.show();
 		} catch(Exception e) {
