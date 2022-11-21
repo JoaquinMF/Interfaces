592ff21bdbfbed2a6d3778874958f710fabf758b
JoaquinMF <kino_1992@hotmail.com>
2022-11-21 14:34:04 +0100

    mensaje

--- com.apuntes2/src/application/IndexController.java
+++ com.apuntes2/src/application/IndexController.java
@@ -4,7 +4,10 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class IndexController {
 	
@@ -21,15 +24,40 @@ public class IndexController {
 	@FXML
 	private TextField txtPaginas;
 	
+	@FXML
+	private TableView <Libro> tableLibros;
 	
+	@FXML
+	private TableColumn <Libro, String> columTitulo;
+	
+	@FXML
+	private TableColumn <Libro, String> columEditorial;
+	
+	@FXML
+	private TableColumn <Libro, String> columAutor;
+	
+	@FXML
+	private TableColumn <Libro, Integer> columPaginas;
+	
+	private ObservableList<Libro> listaLibros=FXCollections.observableArrayList(new Libro("La Biblia","Planeta","Jesus",500));
 	public ObservableList<String> listaEditoriales=FXCollections.observableArrayList("Planeta","Altaya","Kadokawa","Penguin Libros");
+	
 	
 	@FXML
 	private void initialize() {
 		
 		chbEditorial.setItems(listaEditoriales);
 		
+		columTitulo.setCellValueFactory(new PropertyValueFactory<>("titulo"));
+		columEditorial.setCellValueFactory(new PropertyValueFactory<>("editorial"));
+		columAutor.setCellValueFactory(new PropertyValueFactory<>("autor"));
+		columPaginas.setCellValueFactory(new PropertyValueFactory<>("paginas"));
+		
+		tableLibros.setItems(listaLibros);
+		
 	}
+	
+	
 	
 	
 	
